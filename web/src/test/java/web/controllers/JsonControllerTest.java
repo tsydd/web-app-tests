447fd4e705c214172c70eb78808c96404250005c
tsyd <tsyd@tut.by>
2013-10-13 01:09:12 +0300

    mvc/json tests

---
+++ web/src/test/java/web/controllers/JsonControllerTest.java
@@ -0,0 +1,57 @@
+package web.controllers;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+/**
+ * @author Dmitry Tsydzik
+ * @since Date: 13.10.13
+ */
+public class JsonControllerTest {
+
+    @Controller
+    public static class JsonController {
+
+        @RequestMapping("/")
+        @ResponseBody
+        public JsonObject someMethod(@RequestBody JsonObject value) {
+            return value;
+        }
+    }
+
+    public static class JsonObject {
+
+        public String str;
+
+        @SuppressWarnings("unused")
+        public JsonObject() {
+        }
+
+        public JsonObject(String str) {
+            this.str = str;
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+        String value = System.currentTimeMillis() + "";
+        standaloneSetup(new JsonController()).build()
+                .perform(post("/")
+                        .content(new ObjectMapper().writeValueAsString(new JsonObject(value)))
+                        .contentType(MediaType.APPLICATION_JSON))
+//                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.str").value(value));
+    }
+}
