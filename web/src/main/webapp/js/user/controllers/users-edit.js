e1fed69392c300613363d28613281661d2443448
tsyd <tsyd@tut.by>
2013-11-17 15:53:03 +0300

    header, navigation

--- web/src/main/webapp/js/user/controllers/users-edit.js
+++ web/src/main/webapp/js/user/controllers/users-edit.js
@@ -5,13 +5,15 @@ define([
     angular.module('users.edit', [
             'users.service'
         ])
-        .controller('UserEditController', ['$scope', '$routeParams', 'User',
-            function ($scope, $routeParams, User) {
+        .controller('UserEditController', ['$scope', '$location', '$routeParams', 'User',
+            function ($scope, $location, $routeParams, User) {
                 var id = $routeParams.userId;
                 $scope.user = id ? User.get({userId: $routeParams.userId}) : {};
 
                 $scope.save = function () {
-                    User.save($scope.user);
+                    User.save($scope.user, function(user) {
+                        $location.path('/users/' + user.id);
+                    });
                 }
             }])
 });
--- web/src/main/webapp/js/user/controllers/users-list.js
+++ web/src/main/webapp/js/user/controllers/users-list.js
@@ -5,8 +5,12 @@ define([
     angular.module('users.list', [
             'users.service'
         ])
-        .controller('UserListController', ['$scope', 'User',
-            function ($scope, User) {
+        .controller('UserListController', ['$scope', '$location', 'User',
+            function ($scope, $location, User) {
                 $scope.users = User.query();
+
+                $scope.create = function () {
+                    $location.path('/users/edit')
+                }
             }])
 });
