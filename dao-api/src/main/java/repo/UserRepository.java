6dcfacf25926d322b75a845e1eb65208e1a3dae4
tsyd <tsyd@tut.by>
2013-11-22 21:42:04 +0300

    JpaSpecification support

--- dao-api/src/main/java/repo/UserRepository.java
+++ dao-api/src/main/java/repo/UserRepository.java
@@ -2,10 +2,11 @@ package repo;
 
 import domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * @author Dmitry Tsydzik
  * @since Date: 13.10.13
  */
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 }
